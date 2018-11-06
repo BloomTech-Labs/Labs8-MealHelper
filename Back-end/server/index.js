3a65cbb551ba84942ded746abde5063b69d80076
PatrickTheCodeGuy <patrick.jthompson93@gmail.com>
2018-11-06 15:57:02 -0800

    add bcrypt security for register

--- Back-end/server/index.js
+++ Back-end/server/index.js
@@ -5,6 +5,7 @@ const knexConfig = require("./knexfile");
 const db = knex(knexConfig.development);
 const server = express();
 const port = 3300;
+const bcrypt = require("bcrypt");
 
 const cors = require("cors");
 
@@ -19,8 +20,32 @@ server.get("/users", (req, res) => {
 		res.status(200).json(users);
 	});
 });
-
+//Register a new user
 server.post("/register", (req, res) => {
+	const { username, password, zip, healthCondition } = req.body;
+	const user = { username, password, zip, healthCondition };
+	const hash = bcrypt.hashSync(user.password, 15);
+	console.log(hash);
+	user.password = hash;
+	console.log(user);
+	db("users")
+		.insert(user)
+		.then(user => {
+			res.status(201).json(user);
+		});
+});
+
+server.delete("/users/:id", (req, res) => {
+	const { id } = req.params;
+	db("users")
+		.where({ id: id })
+		.del()
+		.then(deleted => {
+			res.status(200).json(deleted);
+		});
+});
+
+server.post("/login", (req, res) => {
 	const { username, password, zip, healthCondition } = req.body;
 	const user = { username, password, zip, healthCondition };
 	console.log(user);
