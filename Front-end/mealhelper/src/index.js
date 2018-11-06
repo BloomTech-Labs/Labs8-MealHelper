b5014d6b963cdee8dbcf352054e02388ae427871
Casey Baker <caseye.baker@gmail.com>
2018-11-06 15:20:07 -0800

    added thunk and logger; added rootReducer; finished index.js

--- Front-end/mealhelper/src/index.js
+++ Front-end/mealhelper/src/index.js
@@ -6,20 +6,20 @@ import * as serviceWorker from './serviceWorker';
 
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-//import thunk from "redux-thunk";
-//import logger from "redux-logger";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import rootReducer from "./store/reducers";
 
-//const store = createStore(rootReducer/*, applyMiddleware(thunk, logger)*/);
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
- // <Provider store={store}>
+  <Provider store={store}>
     <Router>
       <App />
-    </Router>,
-  //</Provider>, 
+    </Router>
+  </Provider>, 
   document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
