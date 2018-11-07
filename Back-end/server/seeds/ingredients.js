c3111508150a47aeb2876f76d83fd6e14b0597d2
PatrickTheCodeGuy <patrick.jthompson93@gmail.com>
2018-11-07 11:14:50 -0800

    add ingredients endpoints, add changes to db to handle ingredients nutrients_id as string of nutrient ids

---
+++ Back-end/server/seeds/ingredients.js
@@ -0,0 +1,29 @@
+exports.seed = function(knex, Promise) {
+	// Deletes ALL existing entries
+	return knex("ingredients")
+		.del()
+		.then(function() {
+			// Inserts seed entries
+			return knex("ingredients").insert([
+				{
+					id: 1,
+					ndb_id: null,
+					name: "Cresco Beef Steak",
+					nutrients_id: "23, 56, 12, 32",
+					user_id: 1
+				},
+				{
+					id: 2,
+					ndb_id: 45,
+					name: "Kraft Sliced Cheese",
+					nutrients_id: "1, 22, 14, 102"
+				},
+				{
+					id: 3,
+					ndb_id: 1056,
+					name: "Pringles Pizza Chips",
+					nutrients_id: "12, 31, 44, 3"
+				}
+			]);
+		});
+};
