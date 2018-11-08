2f834abc1e0effd1300a2de5510fa1bd86fdefa4
jmbradley <jmbradley808@gmail.com>
2018-11-08 15:54:54 -0500

    reformating for signup and login display

--- Front-end/mealhelper/src/App.js
+++ Front-end/mealhelper/src/App.js
@@ -13,11 +13,9 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App">    
         <div className="main-container">
-          <h1>MealHelper</h1>
-          <h3>Under construction</h3>
-          <div className="entry-button-group">
+        <div className="entry-button-group">
           <Link to ="/signup">
             <button className="signup-button">
             <span>Signup</span>
