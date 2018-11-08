e50ea7f8f50e6fca42d941eb14ccd91ffc4d9c95
stdemicheli <stdemicheli@gmail.com>
2018-11-08 01:15:29 +0100

    Refactor Onboarding: split off PersonalInfo view into separate view class.

--- iOS/Labs8-MealHelper/Labs8-MealHelper/Login/OnboardingCollectionViewCell.swift
+++ iOS/Labs8-MealHelper/Labs8-MealHelper/Login/OnboardingCollectionViewCell.swift
@@ -14,7 +14,7 @@ protocol OnboardingCollectionViewCellDelegate {
     func save()
 }
 
-class OnboardingCollectionViewCell: UICollectionViewCell, UITextFieldDelegate {
+class OnboardingCollectionViewCell: UICollectionViewCell {
     
     // MARK: - Properties
     
@@ -137,19 +137,8 @@ class OnboardingCollectionViewCell: UICollectionViewCell, UITextFieldDelegate {
         
         mainStackView.addArrangedSubview(questionLabel)
         addSubview(mainStackView)
-        
-        
-        mainStackView.addArrangedSubview(genderButtonStackView)
-        
-        genders.forEach { (gender) in
-            let button = createButton(for: gender)
-            genderButtonStackView.addArrangedSubview(button)
-        }
-        
-        inputFields.forEach { (input) in
-            let textField = createTextField(for: input)
-            mainStackView.addArrangedSubview(textField)
-        }
+        let personalInfoView = PersonalInfoView()
+        mainStackView.addArrangedSubview(personalInfoView)
         
         mainStackView.anchor(top: headerView.bottomAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: UIEdgeInsets(top: 15.0, left: 15.0, bottom: 150.0, right: 15.0))
         
@@ -161,7 +150,6 @@ class OnboardingCollectionViewCell: UICollectionViewCell, UITextFieldDelegate {
         headerStackView.addArrangedSubview(backButton)
         headerStackView.addArrangedSubview(headerTitleLabel)
         headerStackView.addArrangedSubview(nextButton)
-        //headerView.addSubview(mainStackView)
         
         headerView.anchor(top: safeAreaLayoutGuide.topAnchor, leading: self.leadingAnchor, bottom: nil, trailing: trailingAnchor, padding: UIEdgeInsets(top: 15.0, left: 15.0, bottom: 0, right: 15.0), size: CGSize(width: 0.0, height: 50.0))
         headerStackView.anchor(top: headerView.topAnchor, leading: headerView.leadingAnchor, bottom: headerView.bottomAnchor, trailing: headerView.trailingAnchor)
@@ -173,10 +161,62 @@ class OnboardingCollectionViewCell: UICollectionViewCell, UITextFieldDelegate {
         backButton.addTarget(self, action: #selector(previousCell), for: .touchUpInside)
     }
     
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        
+}
+
+
+class PersonalInfoView: UIView {
+
+    let genders = ["Female", "Male"]
+    let inputFields = ["Username", "Email", "Password", "Birthday", "Height", "Weight"]
+
+    let mainStackView: UIStackView = {
+        let view = UIStackView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.axis = .vertical
+        view.distribution = UIStackView.Distribution.equalCentering
+        view.alignment = .fill
+        return view
+    }()
+
+    private let genderButtonStackView: UIStackView = {
+        let view = UIStackView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.axis = .horizontal
+        view.distribution = .fillEqually
+        view.spacing = 8
+        return view
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
     }
-    
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupViews() {
+        self.addSubview(mainStackView)
+        mainStackView.addArrangedSubview(genderButtonStackView)
+
+        genders.forEach { (gender) in
+            let button = createButton(for: gender)
+            genderButtonStackView.addArrangedSubview(button)
+        }
+
+        inputFields.forEach { (input) in
+            let textField = createTextField(for: input)
+            mainStackView.addArrangedSubview(textField)
+        }
+
+        mainStackView.anchor(top: self.topAnchor, leading: self.leadingAnchor, bottom: self.bottomAnchor, trailing: self.trailingAnchor)
+    }
+
+    @objc func tappedGenderButton() {
+        print("beeep")
+    }
+
     private func createButton(for gender: String) -> UIButton {
         let button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
@@ -191,7 +231,7 @@ class OnboardingCollectionViewCell: UICollectionViewCell, UITextFieldDelegate {
         button.addTarget(self, action: #selector(tappedGenderButton), for: .touchUpInside)
         return button
     }
-    
+
     private func createTextField(for input: String) -> UITextField {
         let tf = UITextField()
         tf.translatesAutoresizingMaskIntoConstraints = false
@@ -206,97 +246,7 @@ class OnboardingCollectionViewCell: UICollectionViewCell, UITextFieldDelegate {
         tf.contentHorizontalAlignment = .left
         tf.heightAnchor.constraint(equalToConstant: 40.0).isActive = true
         tf.isUserInteractionEnabled = true
-        tf.delegate = self
         return tf
-        
     }
-    
-}
 
-//
-//class PersonalInfoView: UIView {
-//
-//    let genders = ["Female", "Male"]
-//    let inputFields = ["Username", "Email", "Password", "Birthday", "Height", "Weight"]
-//
-//    let mainStackView: UIStackView = {
-//        let view = UIStackView()
-//        view.translatesAutoresizingMaskIntoConstraints = false
-//        view.axis = .vertical
-//        view.distribution = UIStackView.Distribution.equalCentering
-//        view.alignment = .fill
-//        return view
-//    }()
-//
-//    private let genderButtonStackView: UIStackView = {
-//        let view = UIStackView()
-//        view.translatesAutoresizingMaskIntoConstraints = false
-//        view.axis = .horizontal
-//        view.distribution = .fillEqually
-//        view.spacing = 8
-//        return view
-//    }()
-//
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//        setupViews()
-//    }
-//
-//    required init?(coder aDecoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//
-//    private func setupViews() {
-//        self.addSubview(mainStackView)
-//        mainStackView.addArrangedSubview(genderButtonStackView)
-//
-//        genders.forEach { (gender) in
-//            let button = createButton(for: gender)
-//            genderButtonStackView.addArrangedSubview(button)
-//        }
-//
-//        inputFields.forEach { (input) in
-//            let textField = createTextField(for: input)
-//            mainStackView.addArrangedSubview(textField)
-//        }
-//
-//        mainStackView.anchor(top: self.topAnchor, leading: self.leadingAnchor, bottom: self.bottomAnchor, trailing: self.trailingAnchor)
-//    }
-//
-//    @objc func tappedGenderButton() {
-//        print("beeep")
-//    }
-//
-//    private func createButton(for gender: String) -> UIButton {
-//        let button = UIButton(type: .system)
-//        button.translatesAutoresizingMaskIntoConstraints = false
-//        button.setTitle(gender, for: .normal)
-//        button.titleLabel?.font = UIFont.systemFont(ofSize: 20.0)
-//        button.layer.cornerRadius = 8.0
-//        button.layer.masksToBounds = true
-//        button.backgroundColor = .red
-//        button.tintColor = .white
-//        //button.widthAnchor.constraint(equalToConstant: 100.0).isActive = true
-//        button.heightAnchor.constraint(equalToConstant: 50.0).isActive = true
-//        button.addTarget(self, action: #selector(tappedGenderButton), for: .touchUpInside)
-//        return button
-//    }
-//
-//    private func createTextField(for input: String) -> UITextField {
-//        let tf = UITextField()
-//        tf.translatesAutoresizingMaskIntoConstraints = false
-//        tf.placeholder = input
-//        tf.font = UIFont.systemFont(ofSize: 17.0)
-//        tf.setLeftPaddingPoints(10.0)
-//        tf.setRightPaddingPoints(10.0)
-//        tf.backgroundColor = .white
-//        tf.tintColor = .blue
-//        tf.layer.cornerRadius = 8.0
-//        tf.layer.masksToBounds = true
-//        tf.contentHorizontalAlignment = .left
-//        tf.heightAnchor.constraint(equalToConstant: 40.0).isActive = true
-//        tf.isUserInteractionEnabled = true
-//        return tf
-//    }
-//
-//}
+}
