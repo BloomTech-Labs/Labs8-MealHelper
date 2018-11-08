c755b455a5e25b33f33e4af51f3b9f772b7070c4
jmbradley <jmbradley808@gmail.com>
2018-11-08 17:06:59 -0500

    finishing login form

--- Front-end/mealhelper/src/components/login/login.js
+++ Front-end/mealhelper/src/components/login/login.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
+import './login.css';
 
 
 class Login extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="main-container">
-          <h1>Login</h1>
+        <div className="form-title">
+        <h1 className="login-title">Login</h1>
+        <form>
+          <div className="form-group">
+              <input type="text" id="dynamic-label-input" placeholder="Email"/>
+                  <label htmlFor="dynamic-label-input">Email</label>
+          </div>
+          <div className="form-group">
+              <input type="password" id="dynamic-label-input" placeholder="Password"/>
+                  <label htmlFor="dynamic-label-input">Password</label>
+          </div>
+        </form>
         </div>
-      </div>
     );
   }
 }
