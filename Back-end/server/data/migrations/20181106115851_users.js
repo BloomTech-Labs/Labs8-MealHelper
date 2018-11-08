077c0420a77c503fdc76fc7ea6cdb239f5779df9
PatrickTheCodeGuy <patrick.jthompson93@gmail.com>
2018-11-08 15:15:40 -0800

    add user id foreign key back

--- Back-end/server/data/migrations/20181106115851_users.js
+++ Back-end/server/data/migrations/20181106115851_users.js
@@ -26,12 +26,11 @@ exports.up = function(knex, Promise) {
 			ingredients.integer("ndb_id");
 			ingredients.string("name", 51).notNullable();
 			ingredients.string("nutrients_id");
-
-			// ingredients
-			// 	.integer("user_id")
-			// 	.references("id")
-			// 	.inTable("users")
-			// 	.onDelete("cascade");
+			ingredients
+				.integer("user_id")
+				.references("id")
+				.inTable("users")
+				.onDelete("cascade");
 		}),
 
 		knex.schema.createTable("nutrients", function(nutrients) {
