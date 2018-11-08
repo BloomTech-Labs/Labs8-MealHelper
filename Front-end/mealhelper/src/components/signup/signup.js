96601d8812361653c36517a416c60db76caf780d
jmbradley <jmbradley808@gmail.com>
2018-11-08 13:37:36 -0500

    signup and login built as classical components

---
+++ Front-end/mealhelper/src/components/signup/signup.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+
+class Signup extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="main-container">
+          <h1>MealHelper</h1>
+          <h3>Under construction</h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Signup;
