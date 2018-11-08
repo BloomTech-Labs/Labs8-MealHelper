f18c7135005a605844d4668ee9e101c7c7e2b6f2
jmbradley <jmbradley808@gmail.com>
2018-11-08 16:44:26 -0500

    styling signup form

--- Front-end/mealhelper/src/components/signup/signup.js
+++ Front-end/mealhelper/src/components/signup/signup.js
@@ -1,14 +1,30 @@
 import React, { Component } from "react";
-
+import './signup.css';
 
 class Signup extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="main-container">
+        <div className="form">
           <h1>Sign Up</h1>
-        </div>
-      </div>
+          <form>
+            <div className="form-group">
+                <input type="text" id="dynamic-label-input" placeholder="Email"/>
+                    <label htmlFor="dynamic-label-input">Email</label>
+            </div>
+            <div className="form-group">
+                <input type="password" id="dynamic-label-input" placeholder="Password"/>
+                    <label htmlFor="dynamic-label-input">Password</label>
+            </div>
+            <div className="form-group">
+                <input type="text" id="dynamic-label-input" placeholder="Zip"/>
+                    <label htmlFor="dynamic-label-input">Zip</label>
+            </div>
+            <div className="form-group">
+                <input type="text" id="dynamic-label-input" placeholder="Health Condition"/>
+                    <label htmlFor="dynamic-label-input">Health Condition</label>
+            </div>
+          </form>
+          </div>
     );
   }
 }
