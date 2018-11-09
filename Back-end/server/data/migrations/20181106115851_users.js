c7f5fcdafab29be5c65f3bea8e5c14221750c8ca
PatrickTheCodeGuy <patrick.jthompson93@gmail.com>
2018-11-08 17:09:16 -0800

    adds user 'id' in increments

--- Back-end/server/data/migrations/20181106115851_users.js
+++ Back-end/server/data/migrations/20181106115851_users.js
@@ -1,6 +1,6 @@
 exports.up = function(knex) {
 	return knex.schema.createTable("users", function(users) {
-		users.increments();
+		users.increments("id").primary();
 		users.string("email", 40).notNullable();
 		users.string("password", 255).notNullable();
 		users.integer("zip", 5);
