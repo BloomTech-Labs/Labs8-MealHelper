b8371da4d010fa9681f5edc97fc0c93cee2b1bb9
stdemicheli <stdemicheli@gmail.com>
2018-11-09 14:24:32 +0100

    Resolved merge conflict. Removed Networking folder.

--- iOS/Labs8-MealHelper /Labs8-MealHelper/Networking/MealClient.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MealClient.swift
-//  Labs8-MealHelper
-//
-//  Created by De MicheliStefano on 08.11.18.
-//  Copyright © 2018 De MicheliStefano. All rights reserved.
-//
-
-import Foundation
