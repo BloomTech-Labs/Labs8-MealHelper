8d191e67d4cb20a7cdc82b8a132a40bbd7dfcef4
PatrickTheCodeGuy <patrick.jthompson93@gmail.com>
2018-11-08 20:52:26 -0800

    adds error message to response

--- Back-end/server/index.js
+++ Back-end/server/index.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken");
 const dbEngine = process.env.DB || "production";
 const knexConfig = require("./knexfile.js")[dbEngine];
 
-const db = knex(knexConfig.production);
+const db = knex(knexConfig);
 const server = express();
 
 const port = process.env.PORT || 3300;
