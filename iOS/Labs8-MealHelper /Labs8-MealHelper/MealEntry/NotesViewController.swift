58db5351823e42fb4a50918011ff82074f385f84
stdemicheli <stdemicheli@gmail.com>
2018-11-09 13:49:19 +0100

    Add base structure for adding notes.

---
+++ iOS/Labs8-MealHelper /Labs8-MealHelper/MealEntry/NotesViewController.swift
@@ -0,0 +1,93 @@
+//
+//  NotesViewController.swift
+//  Labs8-MealHelper
+//
+//  Created by De MicheliStefano on 09.11.18.
+//  Copyright © 2018 De MicheliStefano. All rights reserved.
+//
+
+import UIKit
+
+class NotesViewController: UIViewController {
+
+    // MARK: - Public properties
+    
+    
+    
+    // MARK: - Private properties
+    
+    private let mainStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.spacing = 10.0
+        return stackView
+    }()
+    
+    private let notesTextView: UITextView = {
+        let tv = UITextView()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.font = UIFont.systemFont(ofSize: 17.0)
+        return tv
+    }()
+    
+    private let dateTextField: UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Date"
+        tf.font = UIFont.systemFont(ofSize: 17.0)
+        tf.setLeftPaddingPoints(10.0)
+        tf.setRightPaddingPoints(10.0)
+        tf.backgroundColor = .white
+        tf.tintColor = .blue
+        tf.layer.cornerRadius = 8.0
+        tf.layer.masksToBounds = true
+        tf.contentHorizontalAlignment = .left
+        tf.heightAnchor.constraint(equalToConstant: 40.0).isActive = true
+        return tf
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupViews()
+    }
+    
+    // MARK: - Actions
+    
+    @objc private func doneAddingNotes() {
+        
+    }
+    
+    // MARK: - Private methods
+    
+    private func setupViews() {
+        // Navigation
+        self.title = "Add Notes"
+        let nextBarButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneAddingNotes))
+        navigationItem.setRightBarButton(nextBarButton, animated: true)
+        
+        // Main view
+        view.addSubview(mainStackView)
+        mainStackView.addArrangedSubview(dateTextField)
+        mainStackView.addArrangedSubview(notesTextView)
+        
+        mainStackView.anchor(top: view.layoutMarginsGuide.topAnchor, leading: view.layoutMarginsGuide.leadingAnchor, bottom: view.layoutMarginsGuide.bottomAnchor, trailing: view.layoutMarginsGuide.trailingAnchor, padding: UIEdgeInsets(top: 16.0, left: 0.0, bottom: 0.0, right: 0.0))
+        
+        view.backgroundColor = UIColor.lightGray
+        
+        dateTextField.delegate = self
+        dateTextField.text = dateString(for: Date())
+        createPicker(for: dateTextField)
+    }
+    
+    private func dateString(for date: Date, in locale: Locale = Locale(identifier: Locale.current.identifier)) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        dateFormatter.locale = locale
+        return dateFormatter.string(from: date)
+    }
+
+}
