704f0cded9e9c24874751683ddab6c5dd9dc4b79
PatrickTheCodeGuy <patrick.jthompson93@gmail.com>
2018-11-13 09:49:05 -0800

    adds a change to fix bugs on the post and register endpoints

--- Back-end/server/index.js
+++ Back-end/server/index.js
@@ -70,9 +70,16 @@ server.post("/register", (req, res) => {
 	console.log(user);
 	db("users")
 		.insert(user)
-		.then(user => {
+		.then(userReturn => {
 			const token = generateToken(user);
-			res.status(201).json({ userID: user, token: token });
+			db("users")
+				.where({ email: user.email })
+				.then(user => {
+					res.status(200).json({ userID: user.id, token: token });
+				})
+				.catch(err => {
+					res.status(400).json({ error: "could not grab user" });
+				});
 		})
 		.catch(err => {
 			res.status(400).json({ msg: err, error: "Could not create a user" });
@@ -90,7 +97,7 @@ server.post("/login", (req, res) => {
 			if (user && bcrypt.compareSync(userLogin.password, user.password)) {
 				const token = generateToken(user);
 
-				res.status(200).json({ userID: user, token: token });
+				res.status(200).json({ userID: user.id, token: token });
 			} else {
 				res
 					.status(500)
