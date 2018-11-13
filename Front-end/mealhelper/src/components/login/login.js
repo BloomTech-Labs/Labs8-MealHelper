13cc41bac04f17044c41b0563c45e73a193af1d5
PatrickTheCodeGuy <patrick.jthompson93@gmail.com>
2018-11-13 15:35:43 -0800

    adds login component

--- Front-end/mealhelper/src/components/login/login.js
+++ Front-end/mealhelper/src/components/login/login.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { loginUser } from "../../store/actions/userActions";
@@ -7,48 +6,20 @@ import { Alert } from "reactstrap";
 
 class Login extends Component {
 	constructor(props) {
-=======
-<<<<<<< HEAD
-import React, { Component } from "react";
-import { connect } from 'react-redux';
-import { GETTING_USER } from '../../store/actions';
-import { withRouter } from "react-router-dom";
-=======
-import React from "react";
->>>>>>> 9d607623b715efe5262c6f68b95521792caaa2f8
-import './login.css';
-
-
-const Login = ({ onClick }) => (
-
-<<<<<<< HEAD
-    constructor(props) {
->>>>>>> 78e93935dcc638614756f6352ceae963ce17380a
 		super(props);
 
 		this.state = {
 			email: "",
 			password: "",
-<<<<<<< HEAD
 			visable: false
 		};
 	}
 
 	handleChange = event => {
-=======
-			zip: null,
-			healthCondition: "",
-			visable: false
-		};
-    }
-    
-    handleChange = event => {
->>>>>>> 78e93935dcc638614756f6352ceae963ce17380a
 		event.preventDefault();
 		this.setState({
 			[event.target.name]: event.target.value
 		});
-<<<<<<< HEAD
 	};
 
 	createUser = event => {
@@ -59,23 +30,10 @@ const Login = ({ onClick }) => (
 			const { email, password } = this.state;
 			const user = { email, password };
 			this.props.loginUser(user);
-=======
-	};;
-
-    loginUser = event => {
-        event.preventDefault();
-		if (!this.state.email || !this.state.password) {
-			this.setState({ visable: true });
-		} else {
-			const { email, password, zip, healthCondition } = this.state;
-			const user = { email, password, zip, healthCondition };
-			this.props.addUser(user);
->>>>>>> 78e93935dcc638614756f6352ceae963ce17380a
 			// this.props.history.push("/");
 		}
 	};
 
-<<<<<<< HEAD
 	render() {
 		return (
 			<div className="user-form-container">
@@ -112,56 +70,6 @@ const Login = ({ onClick }) => (
 		);
 	}
 }
-=======
-  render() {
-    return (
-=======
->>>>>>> 9d607623b715efe5262c6f68b95521792caaa2f8
-        <div className="form-title">
-            <h1 className="login-title">Login</h1>
-            <form>
-                <div className="form-group">
-                    <input 
-                        type="text" 
-                        id="dynamic-label-input" 
-                        placeholder="Email"
-                        name="email"
-                        value={this.state.email}
-                        onChange={this.handleChange}
-                        required
-                    />
-                    <label 
-                        htmlFor="dynamic-label-input">
-                        Email
-                    </label>
-                </div>
-                <div className="form-group">
-<<<<<<< HEAD
-                    <input 
-                        type="password" 
-                        id="dynamic-label-input" 
-                        placeholder="Password"
-                        onChange={this.handleChange}
-                        value={this.state.password}
-                        required
-                    />
-                    <label 
-                        htmlFor="dynamic-label-input">
-                        Password
-                    </label>
-=======
-                    <input type="password" id="dynamic-label-input" placeholder="Password" />
-                    <label htmlFor="dynamic-label-input">Password</label>
->>>>>>> 9d607623b715efe5262c6f68b95521792caaa2f8
-                </div>
-                <div className="login login-two" onClick= { onClick }>
-                    <span>Login</span>
-                </div>
-            </form>
-        </div>
-    );
-
->>>>>>> 78e93935dcc638614756f6352ceae963ce17380a
 
 const mapStateToProps = state => ({
 	user: state.user
@@ -169,10 +77,5 @@ const mapStateToProps = state => ({
 
 export default connect(
 	mapStateToProps,
-<<<<<<< HEAD
 	{ loginUser }
 )(withRouter(Login));
-=======
-	{ GETTING_USER }
-)(withRouter(Login));
->>>>>>> 78e93935dcc638614756f6352ceae963ce17380a
