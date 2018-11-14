733343ca02fb2c6bf8906eaccd7eb469faafec53
stdemicheli <stdemicheli@gmail.com>
2018-11-14 12:59:35 +0100

    Adds a food model which includes objects for Meal, Ingredient, and Nutrient.

--- iOS/Labs8-MealHelper /Labs8-MealHelper/Model/Food.swift
+++ iOS/Labs8-MealHelper /Labs8-MealHelper/Model/Food.swift
@@ -7,3 +7,56 @@
 //
 
 import Foundation
+
+struct Meal: Encodable {
+    
+    var identifier: Int?
+    var mealTime: String?
+    var experience: String?
+    var date: String?
+    var userId: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case identifier = "id"
+        case mealTime
+        case experience
+        case date
+        case userId = "user_id"
+    }
+    
+}
+
+struct Ingredient: Encodable {
+    
+    var identifier: Int?
+    var name: String?
+    var nbdId: Int?
+    var userId: Int?
+    var nutrientIds: [Int]?
+    
+    enum CodingKeys: String, CodingKey {
+        case identifier = "id"
+        case nbdId = "nbd_id"
+        case userId = "user_id"
+        case nutrientIds = "nutrients_id"
+    }
+    
+}
+
+struct Nutrient: Encodable {
+    
+    var identifier: String? // usda nutrient id
+    var name: String?
+    var unit: String?
+    var value: String?
+    var gm: Double? // The 100 gram equivalent value for the nutrient
+    
+    enum CodingKeys: String, CodingKey {
+        case identifier = "nutrient_id"
+        case name = "nutrient"
+        case unit
+        case value
+        case gm
+    }
+    
+}
