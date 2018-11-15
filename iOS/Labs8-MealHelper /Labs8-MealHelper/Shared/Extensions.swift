0df361cc8ca82c9f48234d6da63f0a088b6754d1
Simon Elhoej <simonelhoej@gmail.com>
2018-11-15 21:40:27 +0100

    Added extension functions for gradientbackground and dismissal of keyboard

---
+++ iOS/Labs8-MealHelper /Labs8-MealHelper/Shared/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  Labs8-MealHelper
+//
+//  Created by Simon Elhoej Steinmejer on 15/11/18.
+//  Copyright © 2018 De MicheliStefano. All rights reserved.
+//
+
+import Foundation
