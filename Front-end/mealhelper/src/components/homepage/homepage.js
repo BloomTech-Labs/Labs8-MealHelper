faed15b5d8d14a44be52fcf95a1d271a743c9b11
jmbradley <jmbradley808@gmail.com>
2018-11-14 23:19:37 -0500

    successfully hitting API......debugging local state

--- Front-end/mealhelper/src/components/homepage/homepage.js
+++ Front-end/mealhelper/src/components/homepage/homepage.js
@@ -6,6 +6,7 @@ import { addUser } from "../../store/actions/userActions";
 import { withRouter, Link, Route, Switch } from "react-router-dom";
 // import { Alert } from "reactstrap";
 import Weather from "../weather/weather";
+import Recipes from "../recipes/recipes";
 
 class HomePage extends Component {
 	constructor(props) {
@@ -50,22 +51,22 @@ class HomePage extends Component {
                         <h2 className="titlelinks">Recipes</h2>
                     </Link>
                     <Link to="/homepage/alarms" style={{ textDecoration: 'none' }}>
-                    <h2 className="titlelinks">Alarms</h2>
+                        <h2 className="titlelinks">Alarms</h2>
                     </Link>
                     <Link to="/homepage/meals" style={{ textDecoration: 'none' }}>
-                    <h2 className="titlelinks">Meals</h2>
+                        <h2 className="titlelinks">Meals</h2>
                     </Link>
                     <Link to="/homepage/billing" style={{ textDecoration: 'none' }}>
-                    <h2 className="titlelinks">Billing</h2>
+                        <h2 className="titlelinks">Billing</h2>
                     </Link>
                     <Link to="/homepage/settings" style={{ textDecoration: 'none' }}>
-                    <h2 className="titlelinks">Settings</h2>
+                        <h2 className="titlelinks">Settings</h2>
                     </Link>
                 </div>	
                 <div className="dynamic-display">
                     <Switch>
 							<Route path="/homepage/weather" render={() => <Weather />} />
-							{/* <Route path="/homepage/recipes" render={() => <Recipes />} /> */}
+							<Route path="/homepage/recipes" render={() => <Recipes />} />
 							{/* <Route path="/homepage/alarms" render={() => <Alarms />} />
 							<Route path="/homepage/billing" render={() => <Billing />} />
                             <Route path="/homepage/settings" render={() => <Settings />} /> */}
---
+++ Front-end/mealhelper/src/components/recipes/recipes.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+//change the route for this
+import { addUser } from "../../store/actions/userActions";
+import { withRouter} from "react-router-dom";
+// import { Alert } from "reactstrap";
+import axios from "axios";
+import "./recipes.css";
+
+class Recipe extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+            nutrients: []
+
+        }
+	console.log(this.state);
+		};
+    ///converted to Imperial measurement
+    componentDidMount() {
+        axios.get(`https://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=c24xU3JZJhbrgnquXUNlyAGXcysBibSmESbE3Nl6&nutrients=205&nutrients=204&nutrients=208&nutrients=269&ndbno=01009`)
+      .then(response => {
+        this.setState({
+          nutrients: response.data.report.foods[0].name
+    });
+        console.log(response.data.report.foods[0].name); //returns JSON correctly
+        console.log(this.state.nutrients); //returns correct value (304.15)
+    })
+      .catch(error => {
+        console.log('Error', error);
+    });
+}
+
+	handleChange = event => {
+		event.preventDefault();
+		this.setState({
+			[event.target.name]: event.target.value
+		});
+	};
+
+	createUser = event => {
+		event.preventDefault();
+		if (!this.state.email || !this.state.password) {
+			this.setState({ visable: true });
+		} else {
+			const { email, password, zip, healthCondition } = this.state;
+			const user = { email, password, zip, healthCondition };
+			this.props.addUser(user);
+			// this.props.history.push("/");
+		}
+	};
+
+	render() {
+		return (
+            <div className="weather-container">
+			    <div className="recipe-card">
+                    <h1>A Food Name</h1>
+			    </div>
+            </div>
+		);
+	}
+}
+
+const mapStateToProps = state => ({
+	user: state.user
+});
+
+export default connect(
+	mapStateToProps,
+	{ addUser }
+)(withRouter(Recipe));
