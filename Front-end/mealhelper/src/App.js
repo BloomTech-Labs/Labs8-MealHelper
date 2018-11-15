98680ab3fcb029c2fcaa7a6cfd842828b97a940a
jmbradley <jmbradley808@gmail.com>
2018-11-15 13:18:00 -0500

    refactoring lpnav component to be landingpage component

--- Front-end/mealhelper/src/App.js
+++ Front-end/mealhelper/src/App.js
@@ -6,11 +6,10 @@ import { Route, Switch, Link } from "react-router-dom";
 /////Dev. Created/////////
 import Signup from "./components/signup/signup";
 import Login from "./components/login/login";
-import Thanks from "./components/thanks/thanks";
 import HomePage from "./components/homepage/homepage";
 import Weather from "./components/weather/weather";
 import Recipes from "./components/recipes/recipes";
-import LPnav from "./components/landingpagenav/lpnav";
+import LandingPage from "./components/landingpagenav/lpnav";
 ////////////////////////
 
 import "./App.css";
@@ -20,10 +19,9 @@ class App extends Component {
 		return (
 			<div className="App">
 				<Switch>
-					<Route exact path="/" render={() => <LPnav />} />
+					<Route exact path="/" render={() => <LandingPage />} />
 					<Route path="/signup" render={() => <Signup />} />
 					<Route path="/login" render={() => <Login />} />
-					<Route path="/thanks" render={() => <Thanks />} />
 					<Route path="/homepage" render={() => <HomePage />} />
 					<Route path="/homepage/weather" render={() => <Weather />} />
 					<Route path="/homepage/recipes" render={() => <Recipes />} />
