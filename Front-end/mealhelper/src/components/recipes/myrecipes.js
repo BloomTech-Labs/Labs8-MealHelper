1ca8b375d7b1eaf5012a7ea30703d80bf2d2d70d
PatrickTheCodeGuy <patrick.jthompson93@gmail.com>
2018-11-16 08:01:30 -0800

    fixes a bug with netlify deployment

---
+++ Front-end/mealhelper/src/components/recipes/myrecipes.js
@@ -0,0 +1,92 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+//change the route for this
+import { getMeals } from "../../store/actions/mealActions.js";
+import { withRouter, Link, Route } from "react-router-dom";
+// import { Alert } from "reactstrap";
+import axios from "axios";
+import Recipe from "./recipe";
+import "./recipes.css";
+
+class MyRecipes extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			list: [],
+			search: "",
+			name: "",
+			ndbno: null
+		};
+	}
+	componentDidMount() {
+		const id = this.props.user.userID;
+		this.props.getMeals(id);
+	}
+	componentWillReceiveProps(nextProps) {
+		this.setState({ list: nextProps.meals });
+	}
+
+	settingState() {
+		this.setState({ list: this.props.meals });
+	}
+
+	handleChange = event => {
+		event.preventDefault();
+		this.setState({
+			[event.target.name]: event.target.value
+		});
+	};
+
+	render() {
+		return (
+			<div className="weather-container">
+				<div className="home-container">
+					<div className="sidebar">
+						<Link to="/homepage/weather" style={{ textDecoration: "none" }}>
+							<h2 className="titlelinks">Weather</h2>
+						</Link>
+						<Link to="/homepage/recipes" style={{ textDecoration: "none" }}>
+							<h2 className="titlelinks">Recipes</h2>
+						</Link>
+						<Link to="/homepage/alarms" style={{ textDecoration: "none" }}>
+							<h2 className="titlelinks">Alarms</h2>
+						</Link>
+						<Link to="/homepage/meals" style={{ textDecoration: "none" }}>
+							<h2 className="titlelinks">Meals</h2>
+						</Link>
+						<Link to="/homepage/billing" style={{ textDecoration: "none" }}>
+							<h2 className="titlelinks">Billing</h2>
+						</Link>
+						<Link to="/homepage/settings" style={{ textDecoration: "none" }}>
+							<h2 className="titlelinks">Settings</h2>
+						</Link>
+					</div>
+					<div className="dynamic-display">
+						{this.state.list.map(item => (
+							<Recipe
+								item={item}
+								key={item.ndbno}
+								name={item.name}
+								ndbno={item.ndbno}
+							/>
+						))}
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = state => {
+	console.log(state);
+	return {
+		user: state.userReducer.user,
+		meals: state.mealsReducer.meals
+	};
+};
+
+export default connect(
+	mapStateToProps,
+	{ getMeals }
+)(withRouter(MyRecipes));
