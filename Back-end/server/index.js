dfffe5f8de5789bab1fb430304d16c3bddf5f6e9
PatrickTheCodeGuy <patrick.jthompson93@gmail.com>
2018-11-16 09:24:54 -0800

    adds consistency to the auth0 login

--- Back-end/server/index.js
+++ Back-end/server/index.js
@@ -111,7 +111,7 @@ server.post("/registerAuth0", (req, res) => {
 				.then(user => {
 					const token = generateToken(user);
 
-					res.status(200).json({ userID: user, token: token });
+					res.status(200).json({ userID: user.id, token: token });
 				})
 				.catch(err => {
 					res.status(500).json({
