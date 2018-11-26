a991111141041c51e031e8a5700657507b007c0b
Casey Baker <caseye.baker@gmail.com>
2018-11-26 09:50:12 -0800

    boilerplates addAlarm functions and redux

--- Front-end/mealhelper/src/components/alarms/addAlarm.js
+++ Front-end/mealhelper/src/components/alarms/addAlarm.js
@@ -3,3 +3,39 @@ import { connect } from "react-redux";
 import { withRouter } from "react-router-dom"
 import moment from "moment";
 // import actions
+
+class AddAlarm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      
+    }
+  }
+
+  handleChange = event => {
+    event.preventDefault();
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  addAlarm = event => {
+    event.preventDefault();
+    //make sure all required fields are filled in
+    //get fields from state
+    //assign values to alarm body
+    //pass alarm body to addAlarm actions
+    //this.props.history.push to alarms list
+  }
+}
+
+const mapStateToProps = state => ({
+  //map the state
+})
+
+export default connect(
+  mapStateToProps,
+  //action
+)(withRouter(AddAlarm))
+)
