e5706d9ba26bec9651b38e555959c310f47ec2d0
LordOrbnauticus <haywood.d.johnson@gmail.com>
2018-11-26 14:35:57 -0600

    adden axios and ingredient request

---
+++ Front-end/mealhelper/src/components/ingredients/ingredients.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "./getIngredient"
+
+class IngredientList extends Comment {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            
+        }; 
+    }
+}
