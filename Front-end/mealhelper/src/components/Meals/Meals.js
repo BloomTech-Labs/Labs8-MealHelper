20a154fa1eea6701512ea0fae3c46849e5340bd2
PatrickTheCodeGuy <patrick.jthompson93@gmail.com>
2018-11-26 09:04:08 -0800

    adds route fixes and login

--- Front-end/mealhelper/src/components/Meals/Meals.js
+++ Front-end/mealhelper/src/components/Meals/Meals.js
@@ -1,25 +1,19 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 //change the route for this
 import { addWeatherByUser } from "../../store/actions/weatherActions";
 import { withRouter, Link, Route } from "react-router-dom";
 // import { Alert } from "reactstrap";
 import Recipes from "../recipes/recipes";
 import axios from "axios";
-import "./weather.css";
 
 class Weather extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			city: "",
-			zip: "",
-
-			name: "",
-			temp: null,
-			humidity: null,
-			pressure: null
+			meals: []
 		};
 	}
 	///converted to Imperial measurement
@@ -63,6 +57,45 @@ class Weather extends Component {
 					<Link to="/homepage/settings" style={{ textDecoration: "none" }}>
 						<h2 className="titlelinks">Settings</h2>
 					</Link>
+				</div>
+				<div>
+					<Button color="danger" onClick={this.toggle}>
+						+ New Meal
+					</Button>
+					<Modal
+						isOpen={this.state.modal}
+						toggle={this.toggle}
+						className={this.props.className}
+					>
+						<ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+						<ModalBody>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							enim ad minim veniam, quis nostrud exercitation ullamco laboris
+							nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+							reprehenderit in voluptate velit esse cillum dolore eu fugiat
+							nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+							sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</ModalBody>
+						<ModalFooter>
+							<Button color="primary" onClick={this.toggle}>
+								Do Something
+							</Button>{" "}
+							<Button color="secondary" onClick={this.toggle}>
+								Cancel
+							</Button>
+						</ModalFooter>
+					</Modal>
+				</div>
+				<div className="mealList-Display">
+					{/* {this.state.meals.map(meal => (
+						<OneMeal
+							meal={meal}
+							mealTime={meal.mealTime}
+							experience={meal.experience}
+							date={meal.date}
+						/>
+					))} */}
 				</div>
 			</div>
 		);
