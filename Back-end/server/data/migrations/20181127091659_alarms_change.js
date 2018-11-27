d4259f4ddc12bd6649f69aa944b1743a0c442b08
Casey Baker <caseye.baker@gmail.com>
2018-11-27 09:43:40 -0800

    adds migration file to change alarms table fields

---
+++ Back-end/server/data/migrations/20181127091659_alarms_change.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('alarms', function(a) {
+    a.string('label');
+    a.renameColumn('beginTime', 'time');
+    a.dropColumns('endTime', 'beginLimit', 'endLimit', 'repeats');
+});
+};
+
+exports.down = function(knex, Promise) {
+  a.dropColumnIfExists('label');
+  a.renameColumn('time', 'beginTime');
+  a.integer("endTime").notNullable();
+	a.integer("beginLimit").notNullable();
+	a.integer("endLimit").notNullable();
+	a.integer("repeats").notNullable();
+};
