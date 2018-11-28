5e567feb8f7ea0d4ad529181135a721574481147
Casey Baker <caseye.baker@gmail.com>
2018-11-28 14:05:12 -0800

    ran prettier, a thing I forgot existed

--- Front-end/mealhelper/src/components/alarms/addAlarm.js
+++ Front-end/mealhelper/src/components/alarms/addAlarm.js
@@ -1,38 +1,37 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { withRouter } from "react-router-dom"
+import { withRouter } from "react-router-dom";
 //import moment from "moment";
 import Select from "react-select";
 import { addAlarms } from "../../store/actions/alarmActions";
 
-
 //this is embarrassingly not DRY; auto-populate ASAP
 const options = [
-  { value: '0100', label: '1:00 AM' },
-  { value: '0200', label: '2:00 AM' },
-  { value: '0300', label: '3:00 AM' },
-  { value: '0400', label: '4:00 AM' },
-  { value: '0500', label: '5:00 AM' },
-  { value: '0600', label: '6:00 AM' },
-  { value: '0700', label: '7:00 AM' },
-  { value: '0800', label: '8:00 AM' },
-  { value: '0900', label: '9:00 AM' },
-  { value: '1000', label: '10:00 AM' },
-  { value: '1100', label: '11:00 AM' },
-  { value: '1200', label: '12:00 PM' },
-  { value: '1300', label: '1:00 PM' },
-  { value: '1400', label: '2:00 PM' },
-  { value: '1500', label: '3:00 PM' },
-  { value: '1600', label: '4:00 PM' },
-  { value: '1700', label: '5:00 PM' },
-  { value: '1800', label: '6:00 PM' },
-  { value: '1900', label: '7:00 PM' },
-  { value: '2000', label: '8:00 PM' },
-  { value: '2100', label: '9:00 PM' },
-  { value: '2200', label: '10:00 PM' },
-  { value: '2300', label: '11:00 PM' },
-  { value: '2400', label: '12:00 AM (midnight)' },
-]
+  { value: "0100", label: "1:00 AM" },
+  { value: "0200", label: "2:00 AM" },
+  { value: "0300", label: "3:00 AM" },
+  { value: "0400", label: "4:00 AM" },
+  { value: "0500", label: "5:00 AM" },
+  { value: "0600", label: "6:00 AM" },
+  { value: "0700", label: "7:00 AM" },
+  { value: "0800", label: "8:00 AM" },
+  { value: "0900", label: "9:00 AM" },
+  { value: "1000", label: "10:00 AM" },
+  { value: "1100", label: "11:00 AM" },
+  { value: "1200", label: "12:00 PM" },
+  { value: "1300", label: "1:00 PM" },
+  { value: "1400", label: "2:00 PM" },
+  { value: "1500", label: "3:00 PM" },
+  { value: "1600", label: "4:00 PM" },
+  { value: "1700", label: "5:00 PM" },
+  { value: "1800", label: "6:00 PM" },
+  { value: "1900", label: "7:00 PM" },
+  { value: "2000", label: "8:00 PM" },
+  { value: "2100", label: "9:00 PM" },
+  { value: "2200", label: "10:00 PM" },
+  { value: "2300", label: "11:00 PM" },
+  { value: "2400", label: "12:00 AM (midnight)" }
+];
 
 class AddAlarms extends Component {
   constructor(props) {
@@ -42,7 +41,7 @@ class AddAlarms extends Component {
       beginTime: null,
       endTime: null,
       repeats: null
-    }
+    };
   }
 
   handleChange = event => {
@@ -54,77 +53,67 @@ class AddAlarms extends Component {
 
   addAlarm = event => {
     event.preventDefault();
-    console.log("hello!")
-    console.log("start time", this.state.beginTime)
-    console.log("end time", this.state.endTime)
-    console.log("hours between", this.state.repeats)
+    console.log("hello!");
+    console.log("start time", this.state.beginTime);
+    console.log("end time", this.state.endTime);
+    console.log("hours between", this.state.repeats);
     if (!this.state.beginTime || !this.state.endTime || !this.state.repeats) {
       //alert that all fields are required
-    }
-    else {
+    } else {
       let start = Number(this.state.beginTime); //200
       let end = Number(this.state.endTime); //800
-      let repeats = +(this.state.repeats) * 100;
-      console.log("repeats", repeats)
+      let repeats = +this.state.repeats * 100;
+      console.log("repeats", repeats);
       let alarmTimes = [];
-  for (let i = start; i <= end; i+=repeats) {
-    if(i.toString().length === 3) {
-      let time = 0 + i.toString();
-      alarmTimes.push({time: time, label: ""});
-    } else {
-      let time = i.toString();
-      alarmTimes.push({time: time, label: ""});
-    }
-      
-
-      alarmTimes.map(alarm => this.props.addAlarms(alarm))
+      for (let i = start; i <= end; i += repeats) {
+        if (i.toString().length === 3) {
+          let time = 0 + i.toString();
+          alarmTimes.push({ time: time, label: "" });
+        } else {
+          let time = i.toString();
+          alarmTimes.push({ time: time, label: "" });
+        }
+        alarmTimes.map(alarm => this.props.addAlarms(alarm));
       }
-      
-       
-     
-     // this.props.history.push('/homepage/alarms')
-
-     }
- 
+      // this.props.history.push('/homepage/alarms')
     }
-  
-  
+  };
 
   render() {
     return (
       <div className="alarms-container">
-      <div className="home-container">
-      <h1>Add Alarms</h1>
-        <form className="forms">
-        <Select 
-        options={options}
-        className="time"
-        name="beginTime"
-        placeholder="Start Time"
-        onChange={opt => this.setState({ beginTime: opt.value })}
-         />
-      
-        <Select 
-        options={options}
-        className="time"
-        name="endTime"
-        placeholder="End Time"
-        onChange={opt => this.setState({ endTime: opt.value })}
-        />
-          <input
-            className="repeats"
-            name="repeats"
-            value={this.state.repeats}
-            onChange={this.handleChange}
-            placeholder="Hours between each alarm"
-          />
-        </form>
-        <button onClick={this.addAlarm} className="add-alarms btn">
-          Add Alarms
-        </button>
+        <div className="home-container">
+          <h1>Add Alarms</h1>
+          <form className="forms">
+            <Select
+              options={options}
+              className="time"
+              name="beginTime"
+              placeholder="Start Time"
+              onChange={opt => this.setState({ beginTime: opt.value })}
+            />
+
+            <Select
+              options={options}
+              className="time"
+              name="endTime"
+              placeholder="End Time"
+              onChange={opt => this.setState({ endTime: opt.value })}
+            />
+            <input
+              className="repeats"
+              name="repeats"
+              value={this.state.repeats}
+              onChange={this.handleChange}
+              placeholder="Hours between each alarm"
+            />
+          </form>
+          <button onClick={this.addAlarm} className="add-alarms btn">
+            Add Alarms
+          </button>
         </div>
       </div>
-    )
+    );
   }
 }
 
