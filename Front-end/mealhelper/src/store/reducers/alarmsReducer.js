762ed4db9f0b14e0e6e2ce05534b468225fd07b0
Casey Baker <caseye.baker@gmail.com>
2018-11-28 10:51:55 -0800

    adds fetching cases to alarmsReducer

--- Front-end/mealhelper/src/store/reducers/alarmsReducer.js
+++ Front-end/mealhelper/src/store/reducers/alarmsReducer.js
@@ -11,6 +11,14 @@ const initialState = {
   error: null
 };
 
-// export const alarmsReducer = (state = initialState, action) => {
-//   //switch cases
-// }
+export const alarmsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHING_ALARMS:
+      return {...state, fetchingAlarms: true };
+    case FETCHED_ALARMS:
+      return {...state, fetchingAlarms: false, alarms: action.payload }
+    case ALARMS_FETCHING_ERROR:
+      return {...state, fetchingAlarms: false, error: action.payload}
+    
+  }
+}
