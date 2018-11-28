4cb573418b48a2c48c06dd596e69cc2c87e3fc4c
Patrick Thompson <patrick.jthompson93@gmail.com>
2018-11-28 11:01:41 -0800

    adds a timeout to logging in, and adds a signout button

--- Front-end/mealhelper/src/components/Sign.js
+++ Front-end/mealhelper/src/components/Sign.js
@@ -7,38 +7,36 @@ import Googlogo from "../img/google.png";
 import "./Sign.css";
 
 function Sign(props) {
-	const signOut = () => {
-		auth0Client.signOut();
-		props.history.replace("/");
-	};
+  const signOut = () => {
+    auth0Client.signOut();
+    props.history.replace("/");
+  };
 
-	return (
-		<nav>
-			{!auth0Client.isAuthenticated() && (
-				<button className="login-button" onClick={auth0Client.signIn}>
-					<div className="authlogos">
-					<img src={Facelogo} className="authlogo1" alt="Facebook" />
-					<img src={Googlogo} className="authlogo2" alt="Google" />
-					</div>
-				</button>
-			)}
-			{auth0Client.isAuthenticated() && (
-				<div>
-					<label className="mr-2 text-black">
-						{auth0Client.getProfile().name}
-					</label>
-					<button
-						className="login-button"
-						onClick={() => {
-							signOut();
-						}}
-					>
-						Sign Out
-					</button>
-				</div>
-			)}
-		</nav>
-	);
+  return (
+    <nav className="navbar">
+      {!auth0Client.isAuthenticated() && (
+        <button className="btn btn-dark" onClick={auth0Client.signIn}>
+          <img src={Facelogo} className="authlogo1" alt="Facebook" />
+          <img src={Googlogo} className="authlogo2" alt="Google" />
+        </button>
+      )}
+      {auth0Client.isAuthenticated() && (
+        <div>
+          <label className="mr-2 text-black">
+            {auth0Client.getProfile().name}
+          </label>
+          <button
+            className="btn btn-dark"
+            onClick={() => {
+              signOut();
+            }}
+          >
+            Sign Out
+          </button>
+        </div>
+      )}
+    </nav>
+  );
 }
 
 export default withRouter(Sign);
--- Front-end/mealhelper/src/components/signup/signup.js
+++ Front-end/mealhelper/src/components/signup/signup.js
@@ -9,128 +9,128 @@ import { Alert } from "reactstrap";
 import "./signup.css";
 
 class SignUp extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			email: "",
-			password: "",
-			zip: null,
-			healthCondition: "",
-			visable: false
-		};
-	}
+    this.state = {
+      email: "",
+      password: "",
+      zip: null,
+      healthCondition: "",
+      visable: false
+    };
+  }
 
-	handleChange = event => {
-		event.preventDefault();
-		this.setState({
-			[event.target.name]: event.target.value
-		});
-	};
+  handleChange = event => {
+    event.preventDefault();
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
 
-	createUser = event => {
-		event.preventDefault();
-		if (!this.state.email || !this.state.password) {
-			this.setState({ visable: true });
-		} else {
-			const { email, password, zip, healthCondition } = this.state;
-			const user = { email, password, zip, healthCondition };
-			this.props.addUser(user);
-			this.props.history.push("/homepage");
-		}
-	};
+  createUser = event => {
+    event.preventDefault();
+    if (!this.state.email || !this.state.password) {
+      this.setState({ visable: true });
+    } else {
+      const { email, password, zip, healthCondition } = this.state;
+      const user = { email, password, zip, healthCondition };
+      this.props.addUser(user);
+      this.props.history.push("/homepage");
+    }
+  };
 
-	render() {
-		return (
-			<div className="main-container">
-				<div className="formcenter">
-					<div className="user-form-container">
-						<h1 className="signup-title">Sign Up</h1>
-						<form className="signup-form">
-							<div className="form-group">
-								<input
-									id="dynamic-label-input"
-									className="email-input"
-									type="email"
-									name="email"
-									value={this.state.email}
-									onChange={this.handleChange}
-									placeholder="Email"
-								/>
-								<label htmlFor="dynamic-label-input">Email</label>
-							</div>
-							<div className="form-group">
-								<input
-									id="dynamic-label-input"
-									className="password-input"
-									type="password"
-									name="password"
-									onChange={this.handleChange}
-									value={this.state.password}
-									placeholder="Password"
-								/>
-								<label htmlFor="dynamic-label-input">Password</label>
-							</div>
-							<div className="form-group">
-								<input
-									id="dynamic-label-input"
-									className="zip-input"
-									type="number"
-									name="zip"
-									onChange={this.handleChange}
-									value={this.state.zip}
-									placeholder="Zip"
-								/>
-								<label htmlFor="dynamic-label-input">Zip Code</label>
-							</div>
-							<div className="form-group">
-								<input
-									id="dynamic-label-input"
-									className="condition-input"
-									type="text"
-									name="healthCondition"
-									onChange={this.handleChange}
-									value={this.state.healthCondition}
-									placeholder="Health Condition"
-								/>
-								<label htmlFor="dynamic-label-input">Health Condition</label>
-							</div>
-							<div className="signup signup-two" onClick={this.createUser}>
-								<span>Sign Up</span>
-							</div>
-							<div className="auth">
-								<p className="signuptext">Already have an account?</p>
-							</div>
-							<div className="existinguser">
-								<Link to="/login">
-									<button className="login-button">
-										<span>Login</span>
-									</button>
-								</Link>
-								<p className="signuptext">Or</p>
-								<div>
-									<Sign />
-									<Route exact path="/callback" component={Callback} />
-								</div>
-							</div>
-							<div className="alert-box">
-								<Alert isOpen={this.state.visable} color="danger">
-									Please enter an email
-								</Alert>
-							</div>
-						</form>
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="main-container">
+        <div className="formcenter">
+          <div className="user-form-container">
+            <h1 className="signup-title">Sign Up</h1>
+            <form className="signup-form">
+              <div className="form-group">
+                <input
+                  id="dynamic-label-input"
+                  className="email-input"
+                  type="email"
+                  name="email"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                  placeholder="Email"
+                />
+                <label htmlFor="dynamic-label-input">Email</label>
+              </div>
+              <div className="form-group">
+                <input
+                  id="dynamic-label-input"
+                  className="password-input"
+                  type="password"
+                  name="password"
+                  onChange={this.handleChange}
+                  value={this.state.password}
+                  placeholder="Password"
+                />
+                <label htmlFor="dynamic-label-input">Password</label>
+              </div>
+              <div className="form-group">
+                <input
+                  id="dynamic-label-input"
+                  className="zip-input"
+                  type="number"
+                  name="zip"
+                  onChange={this.handleChange}
+                  value={this.state.zip}
+                  placeholder="Zip"
+                />
+                <label htmlFor="dynamic-label-input">Zip Code</label>
+              </div>
+              <div className="form-group">
+                <input
+                  id="dynamic-label-input"
+                  className="condition-input"
+                  type="text"
+                  name="healthCondition"
+                  onChange={this.handleChange}
+                  value={this.state.healthCondition}
+                  placeholder="Health Condition"
+                />
+                <label htmlFor="dynamic-label-input">Health Condition</label>
+              </div>
+              <div className="signup signup-two" onClick={this.createUser}>
+                <span>Sign Up</span>
+              </div>
+              <div className="auth">
+                <p className="signuptext">Already have an account?</p>
+              </div>
+              <div className="existinguser">
+                <Link to="/login">
+                  <button className="login-button">
+                    <span>Login</span>
+                  </button>
+                </Link>
+                <p className="signuptext">Or</p>
+                <div>
+                  <Route exact path="/callback" component={Callback} />
+                  <Sign />
+                </div>
+              </div>
+              <div className="alert-box">
+                <Alert isOpen={this.state.visable} color="danger">
+                  Please enter an email
+                </Alert>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-	user: state.user
+  user: state.user
 });
 
 export default connect(
-	mapStateToProps,
-	{ addUser }
+  mapStateToProps,
+  { addUser }
 )(withRouter(SignUp));
