e1986fae2af63d8ccd168095066c0df9bff5fe7e
Casey Baker <caseye.baker@gmail.com>
2018-11-29 12:11:53 -0800

    finishes up actions, adds back fetching cases in reducer

--- Front-end/mealhelper/src/store/actions/alarmActions.js
+++ Front-end/mealhelper/src/store/actions/alarmActions.js
@@ -8,6 +8,12 @@ export const FETCHING_ALARMS_ERROR = "FETCHING_ALARMS_ERROR";
 export const ADDING_ALARMS = "ADDING_ALARMS";
 export const ADDED_ALARMS = "ADDED_ALARMS";
 export const ADDING_ALARMS_ERROR = "ADDING_ALARMS_ERROR";
+export const DELETING_ALARMS = "ADDING_ALARMS";
+export const DELETED_ALARMS = "ADDED_ALARMS";
+export const DELETING_ALARMS_ERROR = "ADDING_ALARMS_ERROR";
+export const UPDATING_ALARMS = "UPDATING_ALARMS";
+export const UPDATED_ALARMS = "UPDATED_ALARMS";
+export const UPDATING_ALARMS_ERROR = "UPDATING_ALARMS_ERROR";
 
 //Action Creators
 export const fetchAlarms = () => dispatch => {
@@ -24,7 +30,6 @@ export const fetchAlarms = () => dispatch => {
     })
 }
 
-
 export const addAlarms = alarms => dispatch => {
   dispatch({ type: ADDING_ALARMS });
   console.log("ALARMS", alarms);
@@ -38,3 +43,31 @@ export const addAlarms = alarms => dispatch => {
       dispatch({ type: ADDING_ALARMS_ERROR, payload: err });
     });
 }
+
+export const deleteAlarm = id => dispatch => {
+  dispatch({ type: DELETING_ALARM });
+  axios
+    .delete(`URL`)
+    .then(response => {
+      dispatch({ type: DELETED_NOTE, payload: response.data });
+    })
+    .catch(err => {
+      dispatch({ type: DELETED_NOTE_ERROR, payload: err });
+    });
+}
+
+export const updateAlarm = alarm => dispatch => {
+  const id = alarm.id;
+  const label = alarm.label;
+  const alarm = alarm.alarm;
+  const updatedAlarm = { label, alarm };
+  dispatch({ type: UPDATING_ALARM });
+  axios
+    .put( `URL`, updatedAlarm)
+    .then(response => {
+      dispatch({ type: UPDATED_NOTE, payload: response.data });
+    })
+    .catch(err => {
+      dispatch({ type: UPDATING_NOTE_ERROR, payload: err })
+    })
+};
--- Front-end/mealhelper/src/store/reducers/alarmsReducer.js
+++ Front-end/mealhelper/src/store/reducers/alarmsReducer.js
@@ -13,18 +13,19 @@ const initialState = {
 
 export const alarmsReducer = (state = initialState, action) => {
   switch (action.type) {
-    // case FETCHING_ALARMS:
-    //   return {...state, fetchingAlarms: true };
-    // case FETCHED_ALARMS:
-    //   return {...state, fetchingAlarms: false, alarms: action.payload }
-    // case ALARMS_FETCHING_ERROR:
-    //   return {...state, fetchingAlarms: false, error: action.payload}
+    case actionTypes.FETCHING_ALARMS:
+      return {...state, fetchingAlarms: true };
+    case actionTypes.FETCHED_ALARMS:
+      return {...state, fetchingAlarms: false, alarms: action.payload }
+    case actionTypes.ALARMS_FETCHING_ERROR:
+      return {...state, fetchingAlarms: false, error: action.payload}
     case actionTypes.ADDING_ALARMS:
       return {...state, addingAlarms: true };
     case actionTypes.ADDED_ALARMS:
       return {...state, addingAlarms: false, alarms: action.payload };
     case actionTypes.ADDING_ALARMS_ERROR:
       return {...state, addingAlarms: false, error: action.payload};
+    
     default:
       return state;
   }
