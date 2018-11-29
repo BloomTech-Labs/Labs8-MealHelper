0ecded61d8211306ce3b004e1a3dcf18fefdeb00
jmbradley <jmbradley808@gmail.com>
2018-11-29 12:58:16 -0500

    stripe alert

--- Front-end/mealhelper/src/components/checkout/CheckoutForm.js
+++ Front-end/mealhelper/src/components/checkout/CheckoutForm.js
@@ -4,17 +4,22 @@ import { CardElement, injectStripe } from "react-stripe-elements";
 class CheckoutForm extends Component {
   constructor(props) {
     super(props);
-    this.state = { complete: false };
+    this.state = { complete: false, value: "" };
     this.submit = this.submit.bind(this);
   }
 
-  async submit(ev) {
+  //   async
+  submit(ev) {
     // let { token } = await this.props.stripe.createToken({ name: "Name" });
     // let response = await fetch("/charge", {
     //   method: "POST",
     //   headers: { "Content-Type": "text/plain" },
     //   body: token.id
-    this.setState({ complete: true });
+    if (this.state.value) {
+      this.setState({ complete: true });
+    } else {
+      alert("Please enter valid information");
+    }
     // });
 
     // if (response.ok) this.setState({ complete: true });
@@ -25,6 +30,8 @@ class CheckoutForm extends Component {
     return (
       <div className="checkout">
         <p>Would you like to complete the purchase?</p>
+        <span>Name</span>
+        <input value={this.state.value} />
         <CardElement />
         <button onClick={this.submit}>Send</button>
       </div>
