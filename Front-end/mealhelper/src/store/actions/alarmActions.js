fd7533686683ff331182387fc73ac756b87cdd8e
Casey Baker <caseye.baker@gmail.com>
2018-11-29 12:02:53 -0800

    fetchAlarms action

--- Front-end/mealhelper/src/store/actions/alarmActions.js
+++ Front-end/mealhelper/src/store/actions/alarmActions.js
@@ -2,11 +2,29 @@
 import axios from "axios";
 
 //Action Types
+export const FETCHING_ALARMS = "FETCHING_ALARMS";
+export const FETCHED_ALARMS = "FETCHED_ALARMS";
+export const FETCHING_ALARMS_ERROR = "FETCHING_ALARMS_ERROR";
 export const ADDING_ALARMS = "ADDING_ALARMS";
 export const ADDED_ALARMS = "ADDED_ALARMS";
 export const ADDING_ALARMS_ERROR = "ADDING_ALARMS_ERROR";
 
 //Action Creators
+export const fetchAlarms = () => dispatch => {
+  dispatch({ type: FETCHING_ALARMS });
+  const promise = axios.get(
+    "URL"
+  );
+  promise
+    .then(response => {
+      dispatch({ type: FETCHED_ALARMS, payload: response.data });
+    })
+    .catch(err => {
+      dispatch({ type: FETCHING_ALARMS_ERROR, payload: err })
+    })
+}
+
+
 export const addAlarms = alarms => dispatch => {
   dispatch({ type: ADDING_ALARMS });
   console.log("ALARMS", alarms);
