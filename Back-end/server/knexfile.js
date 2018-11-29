0505f1b2c53008bb090e1ea82bbf76c8627048bd
Patrick Thompson <patrick.jthompson93@gmail.com>
2018-11-28 16:49:22 -0800

    fix

--- Back-end/server/knexfile.js
+++ Back-end/server/knexfile.js
@@ -1,38 +1,38 @@
 // Update with your config settings.
 const localPg = {
-	host: process.env.DB_HOST,
-	database: process.env.DB_NAME,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASS
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS
 };
 
-const dbConnection = process.env.DATABASE_URL || localPg;
+const dbConnection = process.env.HEROKU_POSTGRESQL_PURPLE_URL || localPg;
 
 module.exports = {
-	development: {
-		client: "sqlite3",
-		connection: {
-			filename: "./data/db.sqlite3"
-		},
-		useNullAsDefault: true,
-		migrations: {
-			directory: "./data/migrations"
-		}
-	},
-	production: {
-		client: "pg",
-		connection: dbConnection,
-		pool: {
-			min: 2,
-			max: 10
-		},
-		useNullAsDefault: true,
-		migrations: {
-			tableName: "knex_migrations",
-			directory: "./data/migrations"
-		},
-		seeds: {
-			directory: "./seeds"
-		}
-	}
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/db.sqlite3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    }
+  },
+  production: {
+    client: "pg",
+    connection: dbConnection,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    useNullAsDefault: true,
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./seeds"
+    }
+  }
 };
