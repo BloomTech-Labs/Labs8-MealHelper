0b7175b34095e83fdd8cd846ff0db583bddf4a21
Patrick Thompson <patrick.jthompson93@gmail.com>
2018-11-29 09:36:40 -0800

    fixes

--- Back-end/server/knexfile.js
+++ Back-end/server/knexfile.js
@@ -6,7 +6,7 @@ const localPg = {
   password: process.env.DB_PASS
 };
 
-const dbConnection = process.env.HEROKU_POSTGRESQL_PURPLE_URL || localPg;
+const dbConnection = process.env.DATABASE_URL || localPg;
 
 module.exports = {
   development: {
