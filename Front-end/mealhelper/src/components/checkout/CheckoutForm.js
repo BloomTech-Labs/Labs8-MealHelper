58c655fa3b0c009c31a13b8515e45ebbf38e6e88
jmbradley <jmbradley808@gmail.com>
2018-11-29 00:00:25 -0500

    checkoutform is wired for db connect

--- Front-end/mealhelper/src/components/checkout/CheckoutForm.js
+++ Front-end/mealhelper/src/components/checkout/CheckoutForm.js
@@ -4,14 +4,23 @@ import { CardElement, injectStripe } from "react-stripe-elements";
 class CheckoutForm extends Component {
   constructor(props) {
     super(props);
+    this.state = { complete: fales };
     this.submit = this.submit.bind(this);
   }
 
   async submit(ev) {
-    // User clicked submit
+    let { token } = await this.props.stripe.createToken({ name: "Name" });
+    let response = await fetch("/charge", {
+      method: "POST",
+      headers: { "Content-Type": "text/plain" },
+      body: token.id
+    });
+
+    if (response.ok) this.setState({ complete: true });
   }
 
   render() {
+    if (this.state.complete) return <h1>Success! Welcome to HealHelper</h1>;
     return (
       <div className="checkout">
         <p>Would you like to complete the purchase?</p>
