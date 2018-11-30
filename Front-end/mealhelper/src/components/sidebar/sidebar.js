c72bb1896776437d6266e76562e23937e2f79186
jmbradley <jmbradley808@gmail.com>
2018-11-30 09:13:02 -0500

    refactored side bar

--- Front-end/mealhelper/src/components/sidebar/sidebar.js
+++ Front-end/mealhelper/src/components/sidebar/sidebar.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { connect } from "react-redux";
-import "./homepage.css";
-import MealDisplay from "./MealDisplay.js";
-import RecipeDisplay from "./RecipeDisplay.js";
+// import "./homepage.css";
+import MealDisplay from "../homepage/MealDisplay";
+import RecipeDisplay from "../homepage/RecipeDisplay";
 //change the route for this
 import { addUser } from "../../store/actions/userActions";
 import { withRouter, Link, Route, Switch } from "react-router-dom";
@@ -28,9 +28,8 @@ import {
 import { Elements, StripeProvider } from "react-stripe-elements";
 import CheckoutForm from "../checkout/CheckoutForm";
 import Billing from "../billing/billing";
-import SideBar from "../sidebar/sidebar";
 
-class HomePage extends Component {
+class SideBar extends Component {
   constructor(props) {
     super(props);
 
@@ -110,59 +109,75 @@ class HomePage extends Component {
 
   render() {
     return (
-      <div className="home-container-home">
-        <SideBar />
-        <div className="flex-me">
-          <div className="dynamic-display-home-meals">
-            <p className="recentMeals">4 Most Recently Made Meals: </p>
-          </div>
-          <div className="dynamic-display-home">
-            <div className="mealsRecipe-Container">
-              <div>
-                <div className="mealDisplay-Flex">
-                  {this.state.meals
-                    .reverse()
-                    .slice(0, 4)
+      //   <div className="home-container-home">
+      <div className="sidebar">
+        <Link to="/homepage" style={{ textDecoration: "none" }}>
+          <h2 className="titlelinks">Home</h2>
+        </Link>
+        <Link to="/homepage/recipes" style={{ textDecoration: "none" }}>
+          <h2 className="titlelinks">Recipes</h2>
+        </Link>
+        <Link to="/homepage/alarms" style={{ textDecoration: "none" }}>
+          <h2 className="titlelinks">Alarms</h2>
+        </Link>
+        <Link to="/homepage/meals" style={{ textDecoration: "none" }}>
+          <h2 className="titlelinks">Meals</h2>
+        </Link>
+        <Link to="/homepage/billing" style={{ textDecoration: "none" }}>
+          <h2 className="titlelinks">Billing</h2>
+        </Link>
+        <Link to="/homepage/settings" style={{ textDecoration: "none" }}>
+          <h2 className="titlelinks">Settings</h2>
+        </Link>
+        <Button color="danger" onClick={this.toggle}>
+          Log Out
+        </Button>
+        <Link to="homepage/billing">
+          <Button className="danger" color="danger">
+            Upgrade to Premium
+          </Button>
+        </Link>
+        {/* <StripeProvider apiKey="pk_test_rMbD3kGkxVoOsMd0meVqUlmG">
+            <div className="example">
+              <h1>Pay Up Health Nut</h1>
+              <Elements>
+                <CheckoutForm />
+              </Elements>
+            </div>
+          </StripeProvider> */}
 
-                    .map(meal => (
-                      <MealDisplay
-                        key={meal.id}
-                        meal={meal}
-                        mealTime={meal.mealTime}
-                        temp={meal.temp}
-                        experience={meal.experience}
-                        date={meal.date}
-                        city={meal.name}
-                      />
-                    ))}
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="dynamic-display-home-meals">
-            <p className="recentMeals">4 Most Recently Made Recipes: </p>
-          </div>
-          <div className="dynamic-display-home">
-            <div className="mealsRecipe-Container">
-              <div>
-                <div className="mealDisplay-Flex">
-                  {this.state.recipes
-                    .reverse()
-                    .slice(0, 4)
-                    .map(recipe => (
-                      <RecipeDisplay
-                        key={recipe.id}
-                        recipe={recipe}
-                        servings={recipe.servings}
-                        calories={recipe.calories}
-                        name={recipe.name}
-                      />
-                    ))}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Switch>
+          <Route path="/homepage/weather" render={() => <Weather />} />
+          <Route exact path="/homepage/recipes" render={() => <Recipes />} />
+          <Route exact path="/homepage/meals" render={() => <Meals />} />
+          <Route
+            path="/homepage/recipes/createnewrecipe"
+            render={() => <CreateNewRecipe />}
+          />
+          <Route exact path="/homepage/alarms" render={() => <MyAlarms />} />
+          <Route
+            path="/homepage/alarms/add-alarms"
+            render={() => <AddAlarms />}
+          />
+          <Route path="/homepage/billing" render={() => <Billing />} />
+          {/* <Route path="/homepage/settings" render={() => <Settings />} /> */}
+        </Switch>
+
+        <Modal
+          isOpen={this.state.modal}
+          toggle={this.toggle}
+          className={this.props.className}
+        >
+          <ModalHeader toggle={this.toggle}>
+            Do you wish to log out?
+          </ModalHeader>
+          <Button onClick={this.logout} color="danger" className="danger">
+            Log out
+          </Button>
+          <Button onClick={this.toggle} color="primary">
+            Cancel
+          </Button>
+        </Modal>
       </div>
     );
   }
@@ -176,4 +191,4 @@ const mapStateToProps = state => ({
 export default connect(
   mapStateToProps,
   { addUser }
-)(withRouter(HomePage));
+)(withRouter(SideBar));
