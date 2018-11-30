a78e9e903221694365616dda7dd4f3cdb2ea7354
Simon Elhoej <simonelhoej@gmail.com>
2018-11-30 15:24:45 +0100

    Setup a skeleton UI for detail view and some misc changes

--- iOS/Labs8-MealHelper/Labs8-MealHelper/Home/MealDetailViewController.swift
+++ iOS/Labs8-MealHelper/Labs8-MealHelper/Home/MealDetailViewController.swift
@@ -16,6 +16,38 @@ class MealDetailViewController: UIViewController {
         }
     }
     
+    let nutrientsView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.init(white: 0.95, alpha: 1)
+        view.layer.cornerRadius = 12
+        
+        return view
+    }()
+    
+    let ingredientsTableView: UITableView = {
+        let tv = UITableView(frame: .zero, style: .plain)
+        tv.backgroundColor = UIColor.init(white: 0.95, alpha: 1)
+        tv.layer.cornerRadius = 12
+        
+        return tv
+    }()
+    
+    let weatherView: WeatherView = {
+        let wv = WeatherView()
+        wv.backgroundColor = UIColor.init(white: 0.95, alpha: 1)
+        wv.layer.cornerRadius = 12
+        
+        return wv
+    }()
+    
+    let noteView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.init(white: 0.95, alpha: 1)
+        view.layer.cornerRadius = 12
+        
+        return view
+    }()
+    
     let backgroundImageView: UIImageView = {
         let iv = UIImageView(image: #imageLiteral(resourceName: "mountain"))
         iv.contentMode = .scaleAspectFill
@@ -42,27 +74,32 @@ class MealDetailViewController: UIViewController {
     }
     
     override func viewDidLayoutSubviews() {
-        view.setGradientBackground(colorOne: UIColor.nightSkyDark.cgColor, colorTwo: UIColor.nightSkyBlue.cgColor, startPoint: CGPoint(x: 0, y: 0), endPoint: CGPoint(x: 0.8, y: 0.3))
+        view.setGradientBackground(colorOne: UIColor.nightSkyBlue.cgColor, colorTwo: UIColor.mountainDark.cgColor, startPoint: CGPoint(x: 0, y: 0), endPoint: CGPoint(x: 0.8, y: 0.3))
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .clear
         
         setupViews()
     }
     
     private func setupViews() {
         view.addSubview(backgroundImageView)
-//        backgroundImageView.fillSuperview()
-        backgroundImageView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
+        backgroundImageView.fillSuperview()
         
         view.addSubview(blurEffect)
-//        blurEffect.fillSuperview()
-        blurEffect.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
+        blurEffect.fillSuperview()
         
+        view.addSubview(nutrientsView)
+        nutrientsView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: UIEdgeInsets(top: 30, left: 30, bottom: 0, right: 30), size: CGSize(width: 0, height: 100))
         
+        view.addSubview(ingredientsTableView)
+        ingredientsTableView.anchor(top: nutrientsView.bottomAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: UIEdgeInsets(top: 16, left: 30, bottom: 0, right: 30), size: CGSize(width: 0, height: 200))
+        
+        view.addSubview(weatherView)
+        weatherView.anchor(top: ingredientsTableView.bottomAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: UIEdgeInsets(top: 16, left: 30, bottom: 0, right: 30), size: CGSize(width: 0, height: 130))
+        
+        view.addSubview(noteView)
+        noteView.anchor(top: weatherView.bottomAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: UIEdgeInsets(top: 16, left: 30, bottom: 0, right: 30), size: CGSize(width: 0, height: 100))
     }
-    
-    
 }
