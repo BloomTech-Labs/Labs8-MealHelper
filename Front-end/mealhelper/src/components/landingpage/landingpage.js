8c1a47fca7869537d277e5eb8e04bb1f96a81aff
Patrick Thompson <patrick.jthompson93@gmail.com>
2018-12-04 11:58:14 -0800

    changes the landing page style to max width and middle position

--- Front-end/mealhelper/src/components/landingpage/landingpage.js
+++ Front-end/mealhelper/src/components/landingpage/landingpage.js
@@ -3,6 +3,8 @@ import { connect } from "react-redux";
 import { addUser } from "../../store/actions/userActions";
 import { withRouter, Link, Route } from "react-router-dom";
 import Jars from "./Jars.jpg";
+import Computer from "./Computer.jpg";
+import Plate from "./Plate.png";
 import Navbar from "../Navbar/Navbar";
 import "./landingpage.css";
 
@@ -23,26 +25,49 @@ class Landingpage extends Component {
   render() {
     return (
       <div className="main-container">
-        <div>
-          <Navbar />
-        </div>
-        <div className="image-jars-container">
-          <img className="image-jars" src={Jars} alt="No image" />
-        </div>
-        <div className="login-signup-container">
-          <div className="button-container">
-            <button className="button">
-              <p className="button-text" onClick={this.goToSignup}>
-                Sign Up
-              </p>
-            </button>
+        <Navbar />
+        <div className="container">
+          <div className="image-jars-container">
+            <img className="image-jars" src={Jars} alt="No image" />
           </div>
-          <div className="button-container">
-            <button className="button">
-              <p className="button-text" onClick={this.goToLogin}>
-                Log In
-              </p>
-            </button>
+          <div className="login-signup-container">
+            <div className="button-container">
+              <button className="button">
+                <p className="button-text" onClick={this.goToSignup}>
+                  Sign Up
+                </p>
+              </button>
+            </div>
+            <div className="button-container">
+              <button className="button">
+                <p className="button-text" onClick={this.goToLogin}>
+                  Log In
+                </p>
+              </button>
+            </div>
+          </div>
+          <div className="info-container-one">
+            <div className="image-info-container">
+              <img className="image-info" src={Computer} />
+            </div>
+            <div className="card-body">
+              <div className="header-card">
+                <div>
+                  <p className="info-text">Record Your Meals</p>
+                </div>
+                <div className="image-holder">
+                  <div>
+                    <img className="image-info-image" src={Plate} />
+                  </div>
+                </div>
+              </div>
+              <div className="card-body-text-container">
+                <p className="card-body-text">
+                  EatWell makes it easy to make a meal, and write down your
+                  experience, all in one.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
