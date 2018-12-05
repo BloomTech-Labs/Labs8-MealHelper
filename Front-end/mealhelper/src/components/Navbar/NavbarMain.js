98a7379878fadbcff1e75e1f22d338b6cd905b00
Patrick Thompson <patrick.jthompson93@gmail.com>
2018-12-05 10:45:43 -0800

    main nav with dropdown now working

--- Front-end/mealhelper/src/components/Navbar/NavbarMain.js
+++ Front-end/mealhelper/src/components/Navbar/NavbarMain.js
@@ -3,10 +3,22 @@ import { connect } from "react-redux";
 import { withRouter, Link, Route } from "react-router-dom";
 import Hamburger from "./Hamburger.png";
 import Search from "./Search.svg";
-
+import "../../hamburgers.css";
 import "./Navbar.css";
 
 class NavbarLanding extends Component {
+  constructor() {
+    super();
+    this.state = {
+      open: false
+    };
+  }
+  openHamburger = () => {
+    this.setState({
+      open: !this.state.open
+    });
+  };
+
   render() {
     return (
       <div className="Navbar-Container-Main">
@@ -22,8 +34,37 @@ class NavbarLanding extends Component {
           </button>
         </div>
         <div className="hamburger-container">
-          <img className="hamburger" src={Hamburger} />
+          <button
+            onClick={this.openHamburger}
+            className={
+              this.state.open
+                ? "hamburger hamburger--spin is-active"
+                : "hamburger hamburger--spin"
+            }
+          >
+            <span class="hamburger-box">
+              <span class="hamburger-inner" />
+            </span>
+          </button>
+          <div className={this.state.open ? "popper" : "none"} />
+          <ul className={this.state.open ? "menu-list-open" : "menu-list"}>
+            <li className="menu-list-item">1</li>
+            <li className="menu-list-item">2</li>
+            <li className="menu-list-item">3</li>
+            <li className="menu-list-item">4</li>
+            <li className="menu-list-item">5</li>
+          </ul>
         </div>
+        {/* <div className="dd-wrapper">
+          <div className="dd-header">
+            <div className="dd-header-title" />
+          </div>
+          <ul className="dd-list">
+            <li className="dd-list-item" />
+            <li className="dd-list-item" />
+            <li className="dd-list-item" />
+          </ul>
+        </div> */}
       </div>
     );
   }
