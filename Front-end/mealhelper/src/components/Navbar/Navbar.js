5d4d5648344027b58a59e01be342df1d30839fcb
jmbradley <jmbradley808@gmail.com>
2018-12-08 17:33:48 -0500

    adding signup button and links to appropriate routes

--- Front-end/mealhelper/src/components/Navbar/Navbar.js
+++ Front-end/mealhelper/src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
+import { withRouter, Link } from "react-router-dom";
 
 import "./Navbar.css";
 
@@ -9,7 +9,14 @@ class NavbarLanding extends Component {
     return (
       <div className="Navbar-Container">
         <p className="logo">EatWell</p>
-        <button className="login-navbar">Log In</button>
+        <div className="signup-login-buttons-navbar">
+          <Link to="/login" style={{ textDecoration: "none" }}>
+            <button className="buttons-navbar">Log In</button>
+          </Link>
+          <Link to="/signup" style={{ textDecoration: "none" }}>
+            <button className="buttons-navbar">Sign Up</button>
+          </Link>
+        </div>
       </div>
     );
   }
