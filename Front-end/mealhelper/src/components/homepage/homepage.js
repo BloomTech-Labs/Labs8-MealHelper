a17342ae55edb18e057031aa6994572dda005ff6
jmbradley <jmbradley808@gmail.com>
2018-12-10 19:37:33 -0500

    welcome component

--- Front-end/mealhelper/src/components/homepage/homepage.js
+++ Front-end/mealhelper/src/components/homepage/homepage.js
@@ -3,14 +3,27 @@ import { connect } from "react-redux";
 import "./homepage.css";
 import { addUser } from "../../store/actions/userActions";
 import { withRouter } from "react-router-dom";
-import UserHistory from "../display/UserHistory"
+import UserHistory from "../display/UserHistory";
+import Welcome from "../Welcome/welcome";
+
+import { Router, Route } from "react-router";
+
+const routes = (
+  <Route>
+    <Route path="/zip" component={Welcome} />
+  </Route>
+);
+
+Router.run(routes, Router.HashLocation, Root => {
+  React.render(<Root />, document.getElementById("all"));
+});
 
 class HomePage extends Component {
   render() {
     return (
       <div className="home-container-home">
         <div className="display">
-        <UserHistory />
+          <UserHistory />
         </div>
       </div>
     );
