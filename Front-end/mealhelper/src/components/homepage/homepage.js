6b9f49996c070b8f664d3d92fa0515a716c64949
jmbradley <jmbradley808@gmail.com>
2018-12-11 13:18:37 -0500

    css refactor homepage

--- Front-end/mealhelper/src/components/homepage/homepage.js
+++ Front-end/mealhelper/src/components/homepage/homepage.js
@@ -11,11 +11,35 @@ import { Router, Route } from "react-router";
 class HomePage extends Component {
   render() {
     return (
-      <div className="home-container-home">
-        <div className="user-profile-card">
-          <h1 className="user-profile-card-cta">Welcome To EatWell</h1>
+      // <div className="home-container-home">
+      <div className="user-profile-card">
+        <h1 className="user-profile-card-cta">
+          Let's <h1 className="user-profile-card-cta-logo">EatWell</h1>
+          [username]!
+        </h1>
+        <div
+          className="user-profile-card-stats"
+          style={{ textDecoration: "none" }}
+        >
+          <a href="#product" className="stat">
+            <h3>Meals</h3>
+          </a>
+          <a href="#pricing" className="stat">
+            <h3>Recipes</h3>
+          </a>
+          <a href="#team" className="stat">
+            <h3>Alarms</h3>
+          </a>
         </div>
+        <div className="user-profile-card-body">
+          <p className="user-profile-card-body-text">
+            Eat. Track. Analyze. <br />
+            <p className="user-profile-card-body-text-logo">EatWell</p>
+          </p>
+        </div>
+
         {/* <UserHistory /> */}
+        {/* </div> */}
       </div>
     );
   }
