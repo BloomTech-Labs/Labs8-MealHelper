b1cfb28c40aaa50fb2475b8f4d22c6e0485d8928
jmbradley <jmbradley808@gmail.com>
2018-12-10 19:56:43 -0500

    homepage

--- Front-end/mealhelper/src/components/homepage/homepage.js
+++ Front-end/mealhelper/src/components/homepage/homepage.js
@@ -4,19 +4,9 @@ import "./homepage.css";
 import { addUser } from "../../store/actions/userActions";
 import { withRouter } from "react-router-dom";
 import UserHistory from "../display/UserHistory";
-import Welcome from "../Welcome/welcome";
+// import Welcome from "../welcome/welcome";
 
 import { Router, Route } from "react-router";
-
-const routes = (
-  <Route>
-    <Route path="/zip" component={Welcome} />
-  </Route>
-);
-
-Router.run(routes, Router.HashLocation, Root => {
-  React.render(<Root />, document.getElementById("all"));
-});
 
 class HomePage extends Component {
   render() {
