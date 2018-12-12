45f6aa0870d5aecdd4c733e94f4dc89cbdeeb6e4
jmbradley <jmbradley808@gmail.com>
2018-12-11 21:20:53 -0500

    changes to navbar in app

--- Front-end/mealhelper/src/components/getstarted/getstarted.js
+++ Front-end/mealhelper/src/components/getstarted/getstarted.js
@@ -1,21 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import "./homepage.css";
+import "./getstarted.css";
 import { addUser } from "../../store/actions/userActions";
 import { withRouter, Link } from "react-router-dom";
-import UserHistory from "../display/UserHistory";
-import GetStarted from "../getstarted/getstarted";
-// import Welcome from "../welcome/welcome";
 
-import { Router, Route } from "react-router";
-
-class HomePage extends Component {
-  logout = event => {
-    event.preventDefault();
-    localStorage.removeItem("token");
-    localStorage.removeItem("user_id");
-    this.props.history.push("/");
-  };
+class GetStarted extends Component {
   render() {
     return (
       <div className="homepage-main-fence">
@@ -101,7 +90,38 @@ class HomePage extends Component {
               <p className="user-profile-card-body-text-logo">EatWell</p>
             </p>
           </div>
-          {/* <UserHistory /> */}
+        </div>
+        <div>
+          <h1 className="get-started-cta">Get Started!</h1>
+          <div class="inapp-choice-wrapper">
+            <div class="box recipes">
+              Step 1: Recipes
+              <br />
+              <p className="recipes-text">
+                It all starts with the recipe. Click recipe and start adding
+                your ingredients!
+              </p>
+            </div>
+            <div class="box meals">
+              Step 3: Meals <br />
+              <p className="meals-text">
+                Then name your meal and add the recipe.
+              </p>
+            </div>
+            <div class="box ingredients">
+              Step 2: Ingredients <br />
+              <p className="ingredients-text">
+                From your recipe modal, enter your ingredients and save them.
+              </p>
+            </div>
+            <div class="box alarms">
+              Step 4: Alarms
+              <br />
+              <p className="alarms-text">
+                And last, set your alarms to remind you to eat.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     );
@@ -116,4 +136,4 @@ const mapStateToProps = state => ({
 export default connect(
   mapStateToProps,
   { addUser }
-)(withRouter(HomePage));
+)(withRouter(GetStarted));
