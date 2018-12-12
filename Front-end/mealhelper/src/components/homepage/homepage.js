478baa8f8c696cdab6bf1e274ed0faf433bac4df
jmbradley <jmbradley808@gmail.com>
2018-12-11 20:51:58 -0500

    changes to navbar in app

--- Front-end/mealhelper/src/components/homepage/homepage.js
+++ Front-end/mealhelper/src/components/homepage/homepage.js
@@ -4,6 +4,7 @@ import "./homepage.css";
 import { addUser } from "../../store/actions/userActions";
 import { withRouter, Link } from "react-router-dom";
 import UserHistory from "../display/UserHistory";
+import HowTo from "../howto/howto";
 // import Welcome from "../welcome/welcome";
 
 import { Router, Route } from "react-router";
@@ -24,7 +25,7 @@ class HomePage extends Component {
       <div className="homepage-main-fence">
         <div className="user-profile-card">
           <Link to="/homepage/settings" style={{ textDecoration: "none" }}>
-            <div className="user-profile-card-howto">How To:</div>
+            <div className="user-profile-card-howto">Get Started!</div>
           </Link>
           <Link to="/homepage/settings" style={{ textDecoration: "none" }}>
             <div className="user-profile-card-settings">Settings</div>
@@ -107,35 +108,7 @@ class HomePage extends Component {
           {/* <UserHistory /> */}
           {/* </div> */}
         </div>
-        <div class="inapp-choice-wrapper">
-          <div class="box recipes">
-            Step 1: Recipes
-            <br />
-            <p className="recipes-text">
-              It all starts with the recipe. Click recipe and start adding your
-              ingredients!
-            </p>
-          </div>
-          <div class="box meals">
-            Step 3: Meals <br />
-            <p className="meals-text">
-              Then name your meal and add the recipe.
-            </p>
-          </div>
-          <div class="box ingredients">
-            Step 2: Ingredients <br />
-            <p className="ingredients-text">
-              From your recipe modal, enter your ingredients and save them.
-            </p>
-          </div>
-          <div class="box alarms">
-            Step 4: Alarms
-            <br />
-            <p className="alarms-text">
-              And last, set your alarms to remind you to eat.
-            </p>
-          </div>
-        </div>
+        <HowTo />
       </div>
     );
   }
---
+++ Front-end/mealhelper/src/components/howto/howto.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./howto.css";
+import { addUser } from "../../store/actions/userActions";
+import { withRouter, Link } from "react-router-dom";
+
+import { Router, Route } from "react-router";
+
+class HowTo extends Component {
+  render() {
+    return (
+      <div>
+        <div class="inapp-choice-wrapper">
+          <div class="box recipes">
+            Step 1: Recipes
+            <br />
+            <p className="recipes-text">
+              It all starts with the recipe. Click recipe and start adding your
+              ingredients!
+            </p>
+          </div>
+          <div class="box meals">
+            Step 3: Meals <br />
+            <p className="meals-text">
+              Then name your meal and add the recipe.
+            </p>
+          </div>
+          <div class="box ingredients">
+            Step 2: Ingredients <br />
+            <p className="ingredients-text">
+              From your recipe modal, enter your ingredients and save them.
+            </p>
+          </div>
+          <div class="box alarms">
+            Step 4: Alarms
+            <br />
+            <p className="alarms-text">
+              And last, set your alarms to remind you to eat.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  user: state.userReducer.user,
+  meals: state.mealsReducer.meals
+});
+
+export default connect(
+  mapStateToProps,
+  { addUser }
+)(withRouter(HowTo));
