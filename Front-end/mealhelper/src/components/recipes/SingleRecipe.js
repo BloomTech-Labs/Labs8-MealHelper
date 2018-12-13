f8ee9e12746ddeacc00302e0808f636f42a85e02
Patrick Thompson <patrick.jthompson93@gmail.com>
2018-12-13 15:16:58 -0800

    adds single recipe view with delete, edit not yet functional

--- Front-end/mealhelper/src/components/recipes/SingleRecipe.js
+++ Front-end/mealhelper/src/components/recipes/SingleRecipe.js
@@ -25,17 +25,65 @@ class SingleRecipe extends Component {
           }`
         )
         .then(response => {
-          console.log(response);
+          this.setState({ recipe: response.data });
+          const recipe_id = this.state.recipe.id;
+
+          axios
+            .get(
+              `https://labs8-meal-helper.herokuapp.com/ingredients/recipe/${recipe_id}`
+            )
+            .then(response => {
+              this.setState({ ingredients: response.data });
+              axios
+                .get(
+                  `https://labs8-meal-helper.herokuapp.com/nutrients/${recipe_id}`
+                )
+                .then(response => {
+                  this.setState({ nutrition: response.data });
+                });
+            });
         });
     } else {
       this.props.history.push("/");
     }
   }
+  deleteRecipe = () => {
+    const userid = localStorage.getItem("user_id");
+    const id = this.state.recipe.id;
+    this.props.deleteRecipe(id, userid);
+    this.props.history.push("/homepage");
+  };
 
   render() {
     return (
       <div className="single-recipe-container">
-        <h1>Single Recipe View</h1>
+        <div>
+          <h1 className="single-recipe-name">{this.state.recipe.name}</h1>
+          <h2>Servings: {this.state.recipe.servings}</h2>
+        </div>
+        <h2>Ingredients: </h2>
+        <div className="single-recipe-ingredient-container">
+          {this.state.ingredients.map(ingredient => (
+            <div className="ingredient-name">{ingredient.name}</div>
+          ))}
+        </div>
+        <h2>Nutrition: </h2>
+        <div className="single-recipe-nutrient-container">
+          {this.state.nutrition.map(nutrient => (
+            <div className="single-recipe-view-nutrients">
+              {nutrient.nutrient} {nutrient.value} {nutrient.unit}
+            </div>
+          ))}
+        </div>
+        <div className="single-recipe-buttons">
+          <button className="single-recipe-edit-button">Edit Recipe</button>
+          <button
+            className="single-recipe-delete-button"
+            onClick={this.deleteRecipe}
+          >
+            Delete
+          </button>
+        </div>
       </div>
     );
   }
