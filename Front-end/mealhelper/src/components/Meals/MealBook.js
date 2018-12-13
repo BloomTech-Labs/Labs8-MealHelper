d9fa5f9e2c77141188b486c8d8ee5f90b0119d67
Casey Baker <caseye.baker@gmail.com>
2018-12-13 10:53:36 -0800

    dummydata for mealbook build

--- Front-end/mealhelper/src/components/Meals/MealBook.js
+++ Front-end/mealhelper/src/components/Meals/MealBook.js
@@ -8,7 +8,32 @@ import {
 } from "../../store/actions/mealActions";
 // == Styles == //
 
-const meals = [];
+const meals = [
+  {
+    id: 1,
+    user_id: 1,
+    mealTime: "Breakfast",
+    experience: "👍",
+    name: "",
+    temp: null,
+    humidity: null,
+    pressure: null,
+    notes: "a delicious meal",
+    date: "01/01/2018",
+    servings: 1,
+    recipe_id: 1,
+  }
+];
+
+const recipes = [
+  {
+    id: 1,
+    name: "Eggs and Bacon",
+    calories: 400,
+    servings: 1,
+    user_id: 1
+  }
+];
 
 class MealBook extends Component {
   constructor(props) {
@@ -33,3 +58,14 @@ class MealBook extends Component {
     )
   }
 }
+
+const mapStateToProps = state => ({
+  meals: state.mealsReducer.meals,
+  singleMeal: state.mealsReducer.meal,
+  user: state.userReducer.user
+});
+
+export default connect(
+  mapStateToProps,
+  { getMeals }
+)(withRouter(MealBook));
