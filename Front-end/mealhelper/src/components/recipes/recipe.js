8eb98f70cbe989c8adf20107d5e58abf00036ad0
Patrick Thompson <patrick.jthompson93@gmail.com>
2018-12-14 09:35:17 -0800

    fixes deleting on recipe book

--- Front-end/mealhelper/src/components/recipes/recipe.js
+++ Front-end/mealhelper/src/components/recipes/recipe.js
@@ -4,34 +4,40 @@ import { connect } from "react-redux";
 import { addUser } from "../../store/actions/userActions";
 import { withRouter, Link } from "react-router-dom";
 import { getIngredients } from "../../store/actions/ingredActions";
-import { deleteRecipe } from "../../store/actions/recipeActions";
+// import { deleteRecipe } from "../../store/actions/recipeActions";
 
 import "./recipes.css";
 
 class Recipe extends Component {
   render() {
     return (
-      <Link recipeID={this.props.id} to={`/recipe/${this.props.id}`}>
-        <div className="single-recipe-view">
-          <div className="recipe-name">
-            <h1>{this.props.name}</h1>
+      <div className="single-recipe-plz">
+        <Link
+          className="a-tags"
+          recipeID={this.props.id}
+          to={`/recipe/${this.props.id}`}
+        >
+          <div className="single-recipe-view">
+            <div className="recipe-name">
+              <h1>{this.props.name}</h1>
+            </div>
+            <div className="calories-display">
+              Calories: {this.props.calories}
+            </div>
           </div>
-          <div className="calories-display">
-            Calories: {this.props.calories}
-          </div>
-          <button
-            onClick={() =>
-              this.props.deleteRecipe(
-                this.props.id,
-                localStorage.getItem("user_id")
-              )
-            }
-            className="delete-recipe"
-          >
-            Delete
-          </button>
-        </div>
-      </Link>
+        </Link>
+        <button
+          onClick={() =>
+            this.props.deleteRecipe(
+              this.props.id,
+              localStorage.getItem("user_id")
+            )
+          }
+          className="delete-recipe"
+        >
+          Delete
+        </button>
+      </div>
     );
   }
 }
@@ -42,5 +48,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { addUser, deleteRecipe }
+  { addUser }
 )(withRouter(Recipe));
