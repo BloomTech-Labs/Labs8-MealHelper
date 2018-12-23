0df413253a3e3131b2c5c87ad0cb4b25e47cbea6
stdemicheli <stdemicheli@gmail.com>
2018-12-23 12:21:29 +0700

    Sets up watch extension.

---
+++ iOS/Labs8-MealHelper/Labs8-MealHelperWatch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Labs8-MealHelperWatch Extension
+//
+//  Created by De MicheliStefano on 14.12.18.
+//  Copyright © 2018 De MicheliStefano. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ iOS/Labs8-MealHelper/Labs8-MealHelperWatch Extension/WatchClient.swift
@@ -0,0 +1,9 @@
+//
+//  WatchClient.swift
+//  Labs8-MealHelperWatch Extension
+//
+//  Created by De MicheliStefano on 14.12.18.
+//  Copyright © 2018 De MicheliStefano. All rights reserved.
+//
+
+import Foundation
